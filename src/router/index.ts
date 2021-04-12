d29a1ff5195559fe9b2db71b0e89dd27fb7dd587
Axel <am_johansson2@hotmail.com>
2021-04-12 12:14:47 +0200

    reroute invalid path names

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,11 @@ import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    redirect: "/firebase"
+  },
+  {
     path: "/",
     name: "Home",
     component: Home
