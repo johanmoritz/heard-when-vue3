4471f73c11171cbb39dcdf729e250b3cab45beaa
Johan moritz <jmoritz@kth.se>
2021-04-14 15:19:00 +0200

    Adds components for basic music playback

---
+++ src/components/MusicPlayer/MusicPlayerPresenter.vue
@@ -0,0 +1,80 @@
+<template>
+  <MusicPlayerView>
+    <template v-slot:status>
+      <span>{{ msg }}</span>
+    </template>
+    <button :disabled="!canPlay" @click="play">
+      Play
+    </button>
+    <button :disabled="!canPause" @click="pause">
+      Pause
+    </button>
+    <button :disabled="!canConnect" @click="connect">
+      Connect
+    </button>
+  </MusicPlayerView>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import music from "@/store/music";
+import MusicPlayerView from "./MusicPlayerView.vue";
+
+export default defineComponent({
+  components: { MusicPlayerView },
+  props: {
+    songId: String
+  },
+  setup() {
+    const api = music();
+
+    const msg = computed(() => {
+      const current = api.value;
+      return current.kind === "loading"
+        ? "Loading..."
+        : current.kind === "connected"
+        ? "Ready."
+        : current.kind === "error"
+        ? current.message
+        : "Please connect to Spotify.";
+    });
+
+    const canPlay = computed(
+      () => api.value.kind === "connected" && !api.value.playing
+    );
+    const canPause = computed(
+      () => api.value.kind === "connected" && api.value.playing
+    );
+
+    const canConnect = computed(() => api.value.kind !== "loading");
+
+    const play = () => {
+      if (api.value.kind === "connected") {
+        api.value.play();
+      }
+    };
+
+    const pause = () => {
+      if (api.value.kind === "connected") {
+        api.value.pause();
+      }
+    };
+
+    const connect = () => {
+      if (api.value.kind !== "connected" && api.value.kind !== "loading") {
+        api.value.connect();
+      }
+    };
+
+    return {
+      play,
+      pause,
+      connect,
+      msg,
+      canPlay,
+      canPause,
+      canConnect
+    };
+  }
+});
+</script>
