083edbe8d81f63c83b1de9347a4db21b2cc13e2a
Johan moritz <jmoritz@kth.se>
2021-04-14 10:32:51 +0200

    Updates .env validation to include firebase

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,25 @@
 export type CustomEnvVariable =
   | "VUE_APP_PIZZLY_HOST"
-  | "VUE_APP_PIZZLY_PUBLIC_KEY";
+  | "VUE_APP_PIZZLY_PUBLIC_KEY"
+  | "VUE_APP_FIREBASE_API_KEY"
+  | "VUE_APP_FIREBASE_AUTH_DOMAIN"
+  | "VUE_APP_FIREBASE_PROJECT_ID"
+  | "VUE_APP_FIREBASE_STORAGE_BUCKET"
+  | "VUE_APP_FIREBASE_MESSAGING_SENDER_ID"
+  | "VUE_APP_FIREBASE_APP_ID";
 
 export type Environment = { [key in CustomEnvVariable]: string };
 
 export function ValidateEnvironment() {
   const vars: Array<CustomEnvVariable> = [
     "VUE_APP_PIZZLY_HOST",
-    "VUE_APP_PIZZLY_PUBLIC_KEY"
+    "VUE_APP_PIZZLY_PUBLIC_KEY",
+    "VUE_APP_FIREBASE_API_KEY",
+    "VUE_APP_FIREBASE_AUTH_DOMAIN",
+    "VUE_APP_FIREBASE_PROJECT_ID",
+    "VUE_APP_FIREBASE_STORAGE_BUCKET",
+    "VUE_APP_FIREBASE_MESSAGING_SENDER_ID",
+    "VUE_APP_FIREBASE_APP_ID"
   ];
   const env = Object.keys(process.env);
   const missingVariables = vars.filter(v => !env.includes(v));
