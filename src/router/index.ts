1b7056fc69201d227215f6d23bccab1a46d66f4a
Axel <am_johansson2@hotmail.com>
2021-04-16 18:32:41 +0200

    Remove boilerplate files, update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/:pathMatch(.*)*",
-    name: "not-found",
-    redirect: "/firebase"
-  },
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
   {
     path: "/firebase",
     name: "Firebase",
     component: () => import("../views/FirebaseDemo.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    redirect: "/firebase"
   }
 ];
 
