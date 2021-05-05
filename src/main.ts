4aba687e49ffba031c4e6d745e9670f6d282787f
Axel <am_johansson2@hotmail.com>
2021-05-05 14:20:11 +0200

    Help/Instructions modal can be opened/closed, some issues remain

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import store from "./store";
 import router from "./router";
 import { ValidateEnvironment } from "./config";
 import Btn from "./components/Btn.vue";
+import Help from "./components/Help/Help.vue";
 
 ValidateEnvironment();
 
@@ -11,4 +12,5 @@ createApp(App)
   .use(router)
   .use(store)
   .component("Btn", Btn)
+  .component("Help", Help)
   .mount("#app");
