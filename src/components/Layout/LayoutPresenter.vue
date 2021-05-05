c8ef3c85561e25b4db9c438a2610cc237bbe85b7
Johan moritz <jmoritz@kth.se>
2021-05-05 19:46:52 +0200

    Splits App into Presenter and View

---
+++ src/components/Layout/LayoutPresenter.vue
@@ -0,0 +1,48 @@
+<template>
+  <LayoutView>
+    <template v-slot:nav> </template>
+    <template v-slot:main>
+      <router-view />
+    </template>
+
+    <template v-slot:bottom-left>
+      <Error v-if="error" :msg="error" />
+    </template>
+
+    <template v-slot:bottom-right>
+      <Loader v-if="loading" />
+    </template>
+  </LayoutView>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, onUnmounted } from "vue";
+import { useStore } from "vuex";
+import userApi from "@/store/user";
+import { useRouter } from "vue-router";
+import Loader from "@/components/Loader.vue";
+import Error from "@/components/Error.vue";
+import LayoutView from "@/components/Layout/LayoutView.vue";
+
+export default defineComponent({
+  components: { Loader, Error, LayoutView },
+  setup() {
+    const model = useStore();
+    const router = useRouter();
+    const { handleGameUpdates, handleAuthState } = userApi({ model, router });
+
+    const loading = computed(() => model.state.loading as boolean);
+    const error = computed(() => model.state.error as string);
+
+    const unsubModel = handleGameUpdates();
+    const unsubFb = handleAuthState();
+
+    onUnmounted(() => {
+      unsubModel();
+      unsubFb();
+    });
+
+    return { loading, error };
+  }
+});
+</script>
---
+++ src/components/Layout/LayoutView.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="nav">
+    <slot name="nav"> </slot>
+  </div>
+  <slot name="main"></slot>
+  <div id="bottom-left">
+    <slot name="bottom-left"> </slot>
+  </div>
+  <div id="bottom-right">
+    <slot name="bottom-right"></slot>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: rgba(194, 155, 163);
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+.button {
+  background: rgba(243, 215, 220, 0.8);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border: none;
+  color: #2c3e50;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.overlay {
+  position: fixed;
+  display: block;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 2;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+#bottom-right {
+  position: fixed;
+  bottom: 2em;
+  right: 2em;
+}
+
+#bottom-left {
+  position: fixed;
+  bottom: 1em;
+  left: 1em;
+}
+</style>
