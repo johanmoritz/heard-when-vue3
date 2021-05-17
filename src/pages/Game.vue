a02d53dc9d8664614c40a94c444f547cde0d7fbf
Axel <am_johansson2@hotmail.com>
2021-05-17 17:17:13 +0200

    Moved styling to view file

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -75,23 +75,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<style scoped>
-#endButton {
-  position: fixed;
-  bottom: 10%;
-  left: 50%;
-  transform: translate(-50%, 0);
-  -ms-transform: translate(-50%, 0);
-}
-
-@media screen and (max-width: 700px) {
-  #endButton {
-    position: fixed;
-    bottom: 1rem;
-    left: 1rem;
-    transform: translate(0, 0);
-    -ms-transform: translate(0, 0);
-  }
-}
-</style>
